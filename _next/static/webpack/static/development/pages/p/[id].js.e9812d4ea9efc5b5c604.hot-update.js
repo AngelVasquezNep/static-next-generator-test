740302522980b6872dd35352d39e4ae5c403dd1a
Angelito <angel.vasquez.unam@gmail.com>
2020-04-05 21:48:37 -0500

    Second try with getStaticPaths

---
+++ _next/static/webpack/static/development/pages/p/[id].js.e9812d4ea9efc5b5c604.hot-update.js
@@ -0,0 +1,62 @@
+webpackHotUpdate("static/development/pages/p/[id].js",{
+
+/***/ "./pages/p/[id].js":
+/*!*************************!*\
+  !*** ./pages/p/[id].js ***!
+  \*************************/
+/*! exports provided: __N_SSG, default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "__N_SSG", function() { return __N_SSG; });
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _components_MyLayout__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../components/MyLayout */ "./components/MyLayout.js");
+var _this = undefined,
+    _jsxFileName = "/Users/angelito/development/personal/nextJsProject/next-learn-demo/6-fetching-data/pages/p/[id].js";
+
+
+var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
+
+
+var Post = function Post(props) {
+  return __jsx(_components_MyLayout__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 5,
+      columnNumber: 3
+    }
+  }, __jsx("h1", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6,
+      columnNumber: 5
+    }
+  }, props.show.name), __jsx("p", {
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7,
+      columnNumber: 5
+    }
+  }, props.show.summary.replace(/<[/]?[pb]>/g, "")), props.show.image ? __jsx("img", {
+    src: props.show.image.medium,
+    __self: _this,
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 8,
+      columnNumber: 25
+    }
+  }) : null);
+};
+
+var __N_SSG = true;
+/* harmony default export */ __webpack_exports__["default"] = (Post);
+
+/***/ })
+
+})
+//# sourceMappingURL=[id].js.e9812d4ea9efc5b5c604.hot-update.js.map
